a2a8d4aed6e0c034e52a1a9039d8ea3ddb7a21d4
Archis <archismanpaul09@outlook.com>
2024-09-22 23:17:15 +0530

    Update Sample.c

--- Sample.c
+++ Sample.c
@@ -1,48 +1,46 @@
 #include <stdio.h>
-void inputMatrix(int rows, int cols, int matrix[rows][cols]);
-void addMatrices(int rows, int cols, int matrix1[rows][cols], int matrix2[rows][cols], int sum[rows][cols]);
-void displayMatrix(int rows, int cols, int matrix[rows][cols]);
 
-int main() {
-    int rows, cols;
-    printf("Enter the number of rows: ");
-    scanf("%d", &rows);
-    printf("Enter the number of columns: ");
-    scanf("%d", &cols);
-    
-    int matrix1[rows][cols], matrix2[rows][cols], sum[rows][cols];
-    printf("Enter elements of first matrix:\n");
-    inputMatrix(rows, cols, matrix1);
-    printf("Enter elements of second matrix:\n");
-    inputMatrix(rows, cols, matrix2);
-    
-    addMatrices(rows, cols, matrix1, matrix2, sum);
-    
-    printf("Sum of the two matrices:\n");
-    displayMatrix(rows, cols, sum);
-
-    return 0;
-}
-void inputMatrix(int rows, int cols, int matrix[rows][cols]) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("Enter element at [%d][%d]: ", i, j);
+void input(int r, int c, int matrix[r][c], const char* name) {
+    printf("Enter elements of the %s Matrix:\n", name);
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
 }
-void addMatrices(int rows, int cols, int matrix1[rows][cols], int matrix2[rows][cols], int sum[rows][cols]) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+
+void calculate(int r, int c, int matrix1[r][c], int matrix2[r][c], int result[r][c]) {
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
 }
-void displayMatrix(int rows, int cols, int matrix[rows][cols]) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d ", matrix[i][j]);
+
+void display(int r, int c, int matrix[r][c]) {
+    printf("Output Matrix:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d\t", matrix[i][j]);
         }
         printf("\n");
     }
+}
+
+int main() {
+    int r, c;
+    printf("Enter the number of rows and columns of the matrices:\n");
+    scanf("%d %d", &r, &c);
+    if (r > 0 && c > 0) {
+
+        int matrix1[r][c], matrix2[r][c], result[r][c];
+        input(r, c, matrix1, "1st");
+        input(r, c, matrix2, "2nd");
+
+        calculate(r, c, matrix1, matrix2, result);
+        display(r, c, result);
+    } else {
+        printf("Error: Rows and columns should be greater than 0\n");
+    }
+    return 0;
 }
