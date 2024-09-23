6c17a099f099e794d35c532c89ffab24fd8a0bcd
Archis <archismanpaul09@outlook.com>
2024-09-23 15:05:14 +0530

    Update Sample.cpp

--- Sample.cpp
+++ Sample.cpp
@@ -1,55 +1,72 @@
 #include <iostream>
 using namespace std;
-class MatrixSum {
+class MatrixSum
+{
     int r, c;
-    int** x;
-    int** y;
-    int** z;
+    int **x;
+    int **y;
+    int **z;
+
 public:
-    MatrixSum(int r1, int c1) {
+    MatrixSum(int r1, int c1)
+    {
         r = r1;
         c = c1;
-        x = new int*[r];
-        y = new int*[r];
-        z = new int*[r];
-        for (int i = 0; i < r; i++) {
+        x = new int *[r];
+        y = new int *[r];
+        z = new int *[r];
+        for (int i = 0; i < r; i++)
+        {
             x[i] = new int[c];
             y[i] = new int[c];
             z[i] = new int[c];
         }
     }
-    void input() {
+    void input()
+    {
         cout << "Enter elements of the 1st Matrix:" << endl;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
                 cin >> x[i][j];
             }
         }
         cout << "Enter elements of the 2nd Matrix:" << endl;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
                 cin >> y[i][j];
             }
         }
     }
-    void calculate() {
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+    void calculate()
+    {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
                 z[i][j] = x[i][j] + y[i][j];
             }
         }
     }
-    void display() {
+    void display()
+    {
         cout << "Output Matrix:" << endl;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
                 cout << z[i][j] << "\t";
             }
             cout << endl;
         }
     }
-    ~MatrixSum() {
-        for (int i = 0; i < r; i++) {
+    ~MatrixSum()
+    {
+        for (int i = 0; i < r; i++)
+        {
             delete[] x[i];
             delete[] y[i];
             delete[] z[i];
@@ -59,17 +76,20 @@ public:
         delete[] z;
     }
 };
-int main() {
+int main()
+{
     int r, c;
     cout << "Enter the number of rows and columns of the matrices:" << endl;
     cin >> r >> c;
-
-    if (r > 0 && c > 0) {
+    if (r > 0 && c > 0)
+    {
         MatrixSum obj(r, c);
         obj.input();
         obj.calculate();
         obj.display();
-    } else {
+    }
+    else
+    {
         cout << "Error: Rows and columns should be greater than 0" << endl;
     }
     return 0;
